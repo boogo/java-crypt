110ea874cb082c60386b5c11c99b03c9ade23ebc
james <james@localhost.localdomain>
2016-06-06 10:57:34 +0800

    init

---
+++ src/main/java/com/zhaiqianfeng/disc/java_crypt/App.java
@@ -0,0 +1,13 @@
+package com.zhaiqianfeng.disc.java_crypt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
