c5e3ee09442a9e77112ae7c3110eb8b194c0a463
zhaiqianfeng <zhaiqianfeng@163.com>
2016-06-07 22:09:46 +0800

    3DES

--- src/main/java/com/zhaiqianfeng/disc/crypt/LabSymmetricCipher.java
+++ src/main/java/com/zhaiqianfeng/disc/crypt/LabSymmetricCipher.java
@@ -12,6 +12,8 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.bouncycastle.util.encoders.Hex;
@@ -24,8 +26,10 @@ public class LabSymmetricCipher {
 	private static String src="com.zhaiqiafneng";
 	public static void main(String[] args){
 		jdkDES();
+		jdk3DES();
 	}
 	
+	//jdk DES
 	public static void jdkDES(){
 		try {
 			//生成Key
@@ -53,6 +57,40 @@ public class LabSymmetricCipher {
 			cipher.init(Cipher.DECRYPT_MODE, dESKey);
 			byte[] decodeDESRes=cipher.doFinal(encodeDESRes);
 			System.out.println("jdk DES decode:"+new String(decodeDESRes));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	//jdk 3DES
+	public static void jdk3DES(){
+		try {
+			//生成Key
+			KeyGenerator keyGenerator=KeyGenerator.getInstance("DESede");
+			keyGenerator.init(112);
+			Key secretKey=keyGenerator.generateKey();
+			byte[] byteKey=secretKey.getEncoded();
+			
+			//赋予Key特性，获取特性的Key有两种方式
+			//方式一
+//			DESKeySpec dESKeySpec=new DESKeySpec(byteKey);
+//			SecretKeyFactory factory=SecretKeyFactory.getInstance("DESede");
+//			Key dESKey=factory.generateSecret(dESKeySpec);
+			
+//			//方式二
+			Key dESKey=new SecretKeySpec(byteKey,"DESede");
+			
+			//加密
+			Cipher cipher=Cipher.getInstance("DESede/ECB/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, dESKey);
+			byte[] encodeDESRes=cipher.doFinal(src.getBytes());
+			System.out.println("jdk 3DES encode:"+Hex.toHexString(encodeDESRes));
+			
+			//解密
+			cipher.init(Cipher.DECRYPT_MODE, dESKey);
+			byte[] decodeDESRes=cipher.doFinal(encodeDESRes);
+			System.out.println("jdk 3DES decode:"+new String(decodeDESRes));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
