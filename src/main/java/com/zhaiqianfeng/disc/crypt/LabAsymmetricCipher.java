bfef3f97851a8a5a286e191c8503a1e928a7d4c9
zhaiqianfeng <zhaiqianfeng@163.com>
2016-06-08 09:10:24 +0800

    RSA

--- src/main/java/com/zhaiqianfeng/disc/crypt/LabAsymmetricCipher.java
+++ src/main/java/com/zhaiqianfeng/disc/crypt/LabAsymmetricCipher.java
@@ -14,6 +14,8 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -40,16 +42,24 @@ public class LabAsymmetricCipher {
 			PrivateKey rsaPrivateKey=keyPair.getPrivate();
 			
 			//获取规范秘钥
-//			Key privateKey=new SecretKeySpec(rsaPrivateKey.getEncoded(),"RSA");
+			//Only RSAPrivate(Crt)KeySpec and PKCS8EncodedKeySpec supported for RSA private keys
 			PKCS8EncodedKeySpec pkcs8EncodeKeySpec=new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
 			KeyFactory keyFactory=KeyFactory.getInstance("RSA");
 			Key privateKey=keyFactory.generatePrivate(pkcs8EncodeKeySpec);
 			
-			//私钥加密 公钥解密
+			//私钥加密 公钥解密之私钥加密
 			Cipher cipher=Cipher.getInstance("RSA");
 			cipher.init(Cipher.ENCRYPT_MODE, privateKey);
 			byte[] encodeRes=cipher.doFinal(src.getBytes());
 			System.out.println("私钥加密结果："+Hex.toHexString(encodeRes));
+			
+			//私钥加密 公钥解密之公钥解密
+			//Only RSAPublicKeySpec and X509EncodedKeySpec supported for RSA public keys
+			X509EncodedKeySpec x509EncodedKeySpec=new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+			Key publicKey=keyFactory.generatePublic(x509EncodedKeySpec);
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
+			byte[] decodeRes=cipher.doFinal(encodeRes);
+			System.out.println("公钥解密结果："+new String(decodeRes));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
