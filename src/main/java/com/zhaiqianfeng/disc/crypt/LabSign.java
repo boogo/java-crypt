6f242183d34fdf43ef7e6435be9ae8c8099ed998
zhaiqianfeng <zhaiqianfeng@163.com>
2016-06-10 10:54:31 +0800

    ECDSA

--- src/main/java/com/zhaiqianfeng/disc/crypt/LabSign.java
+++ src/main/java/com/zhaiqianfeng/disc/crypt/LabSign.java
@@ -18,7 +18,6 @@ import java.security.spec.X509EncodedKeySpec;
 
 import org.bouncycastle.util.encoders.Hex;
 
-
 /**
  * 签名算法
  */
@@ -26,76 +25,110 @@ public class LabSign {
 
 	// 待处理的字符串
 	private static String src = "com.zhaiqianfeng";
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		jdkRSASigin();
 		jdkDSASigin();
+		jdkECDASigin();
 	}
-	
-	//jdk 实现的RSA签名算法
-	public static void jdkRSASigin(){
+
+	// jdk 实现的RSA签名算法
+	public static void jdkRSASigin() {
 		try {
-			//构建秘钥
-			KeyPairGenerator keyPairGenerator=KeyPairGenerator.getInstance("RSA");
+			// 构建秘钥
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
 			keyPairGenerator.initialize(512);
-			KeyPair keyPair=keyPairGenerator.generateKeyPair();
-			
-			//构建符合规范的秘钥
-			KeyFactory keyFactory=KeyFactory.getInstance("RSA");
-			PKCS8EncodedKeySpec pkcs8EncodedKeySpec=new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
-			PrivateKey privateKey=keyFactory.generatePrivate(pkcs8EncodedKeySpec);
-			
-			//私钥签名
-			Signature signature=Signature.getInstance("MD5withRSA");
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+			// 构建符合规范的秘钥
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
+			PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+			// 私钥签名
+			Signature signature = Signature.getInstance("MD5withRSA");
 			signature.initSign(privateKey);
 			signature.update(src.getBytes());
-			byte[] encodeResult=signature.sign();
-			System.out.println("MD5withRSA签名结果："+Hex.toHexString(encodeResult));
-			
-			//公钥验证签名
-			X509EncodedKeySpec X509EncodedKeySpec=new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
-			PublicKey publicKey=keyFactory.generatePublic(X509EncodedKeySpec);
+			byte[] encodeResult = signature.sign();
+			System.out.println("MD5withRSA签名结果：" + Hex.toHexString(encodeResult));
+
+			// 公钥验证签名
+			X509EncodedKeySpec X509EncodedKeySpec = new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
+			PublicKey publicKey = keyFactory.generatePublic(X509EncodedKeySpec);
 			signature.initVerify(publicKey);
 			signature.update(src.getBytes());
-			boolean verfyResult=signature.verify(encodeResult);
-			System.out.println("MD5withRSA验证签名结果："+verfyResult);
+			boolean verfyResult = signature.verify(encodeResult);
+			System.out.println("MD5withRSA验证签名结果：" + verfyResult);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	//jdk 实现的DSA签名算法
-		public static void jdkDSASigin(){
-			try {
-				//构建秘钥
-				KeyPairGenerator keyPairGenerator=KeyPairGenerator.getInstance("DSA");
-				keyPairGenerator.initialize(512);
-				KeyPair keyPair=keyPairGenerator.generateKeyPair();
-				
-				//构建符合规范的秘钥
-				KeyFactory keyFactory=KeyFactory.getInstance("DSA");
-				PKCS8EncodedKeySpec pkcs8EncodedKeySpec=new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
-				PrivateKey privateKey=keyFactory.generatePrivate(pkcs8EncodedKeySpec);
-				
-				//私钥签名
-				Signature signature=Signature.getInstance("SHA1withDSA");
-				signature.initSign(privateKey);
-				signature.update(src.getBytes());
-				byte[] encodeResult=signature.sign();
-				System.out.println("\r\nSHA1withDSA签名结果："+Hex.toHexString(encodeResult));
-				
-				//公钥验证签名
-				X509EncodedKeySpec X509EncodedKeySpec=new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
-				PublicKey publicKey=keyFactory.generatePublic(X509EncodedKeySpec);
-				signature.initVerify(publicKey);
-				signature.update(src.getBytes());
-				boolean verfyResult=signature.verify(encodeResult);
-				System.out.println("SHA1withDSA验证签名结果："+verfyResult);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
+	// jdk 实现的DSA签名算法
+	public static void jdkDSASigin() {
+		try {
+			// 构建秘钥
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+			keyPairGenerator.initialize(512);
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+			// 构建符合规范的秘钥
+			KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
+			PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+			// 私钥签名
+			Signature signature = Signature.getInstance("SHA1withDSA");
+			signature.initSign(privateKey);
+			signature.update(src.getBytes());
+			byte[] encodeResult = signature.sign();
+			System.out.println("\r\nSHA1withDSA签名结果：" + Hex.toHexString(encodeResult));
+
+			// 公钥验证签名
+			X509EncodedKeySpec X509EncodedKeySpec = new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
+			PublicKey publicKey = keyFactory.generatePublic(X509EncodedKeySpec);
+			signature.initVerify(publicKey);
+			signature.update(src.getBytes());
+			boolean verfyResult = signature.verify(encodeResult);
+			System.out.println("SHA1withDSA验证签名结果：" + verfyResult);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+	}
+
+	// jdk 实现的ECDSA签名算法
+	public static void jdkECDASigin() {
+		try {
+			// 构建秘钥
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+			keyPairGenerator.initialize(112);
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+			// 构建符合规范的秘钥
+			KeyFactory keyFactory = KeyFactory.getInstance("EC");
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyPair.getPrivate().getEncoded());
+			PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+			// 私钥签名
+			Signature signature = Signature.getInstance("SHA1withECDSA");
+			signature.initSign(privateKey);
+			signature.update(src.getBytes());
+			byte[] encodeResult = signature.sign();
+			System.out.println("\r\nSHA1withECDSA签名结果：" + Hex.toHexString(encodeResult));
+
+			// 公钥验证签名
+			X509EncodedKeySpec X509EncodedKeySpec = new X509EncodedKeySpec(keyPair.getPublic().getEncoded());
+			PublicKey publicKey = keyFactory.generatePublic(X509EncodedKeySpec);
+			signature.initVerify(publicKey);
+			signature.update(src.getBytes());
+			boolean verfyResult = signature.verify(encodeResult);
+			System.out.println("SHA1withECDSA验证签名结果：" + verfyResult);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
